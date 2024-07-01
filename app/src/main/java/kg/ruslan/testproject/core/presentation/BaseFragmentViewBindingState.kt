f9b534800e563352d0af2640934dae70277cf140
Ruslan <ruslan@Ruslans-MacBook-Pro-2.local>
2024-07-01 18:38:51 +0600

    added base classes for presentation layer

---
+++ app/src/main/java/kg/ruslan/testproject/core/presentation/BaseFragmentViewBindingState.kt
@@ -0,0 +1,126 @@
+package kg.ruslan.testproject.core.presentation
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+/**
+ * [BaseFragmentViewBindingState] is a base class to extend from your View (MVVM)
+ * It contains some state implementation.
+ *
+ * @param UIState is a state of your view
+ * @param UIIntents is some actions from your view to handle in ViewModel
+ * @param Binding is binding of your xml file. Initialized all of your views
+ * @param ViewM is your ViewModel (MVVM)
+ * @param inflate is member function from your [Binding] to create instance of your xml file
+ *
+ * @author Ruslan
+ * @see BaseStateViewModel
+ */
+abstract class BaseFragmentViewBindingState<
+		UIState : BaseUIState,
+		UIIntents,
+		Binding : ViewBinding,
+		ViewM : BaseStateViewModel<UIState, UIIntents>,
+		>(
+	private val inflate: Inflate<Binding>,
+) : Fragment() {
+	
+	private var _binding: Binding? = null
+	protected val binding get() = _binding!!
+	
+	abstract val viewModel: ViewM
+	
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?,
+	): View? {
+		_binding = inflate.invoke(inflater, container, false)
+		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initialize()
+	}
+	
+	/**
+	 * [initialize] is member function to initialize some fragment's stuff if needed.
+	 * Called immediately after [onCreateView].
+	 *
+	 * !Do not delete super call after overriding!
+	 */
+	protected open fun initialize() {
+		viewModel.state.partialListener(::showError) { it.showError }
+		viewModel.progressLV.observe(viewLifecycleOwner, ::showProgress)
+	}
+	
+	/**
+	 * Member function [showProgress] is calling to show progress in the ui layer
+	 *
+	 * @param isProgress State of Progress.
+	 *
+	 * True if you need to show progress.
+	 * False if you need to dismiss progress.
+	 */
+	open fun showProgress(isProgress: Boolean) {}
+	
+	/**
+	 * This method calling [showErrorAlert] if param message is not null
+	 *
+	 * @param message is some message to be displayed
+	 * @see showErrorAlert
+	 */
+	private fun showError(message: String? = null) {
+		if (message != null)
+			showErrorAlert(message)
+	}
+	
+	/**
+	 * [showErrorAlert] is member function to show some error message in [AlertDialog]
+	 * on the screen
+	 *
+	 * @param message is some message to be displayed
+	 */
+	protected open fun showErrorAlert(message: String) {
+		Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+	}
+	
+	/**
+	 * [partialListener] is a extension function on [StateFlow]
+	 * Using to set some listener on changing some property in the state.
+	 * high-order function calling in [Dispatchers.Main] thread
+	 *
+	 * @param T is property you need to listen
+	 * @param block is high-order function which is called on change property-you-need
+	 * @param transform is high-order function which called to figure out which property you need.
+	 * Within param value which is [UIState] and returning [T] which needs to be property of [UIState]
+	 */
+	protected inline fun <T> StateFlow<UIState>.partialListener(
+		crossinline block: (T) -> Unit,
+		crossinline transform: suspend (value: UIState) -> T,
+	) {
+		viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+			map(transform)
+				.distinctUntilChanged()
+				.collect {
+					block(it)
+				}
+		}
+	}
+	
+}
---
+++ app/src/main/java/kg/ruslan/testproject/core/presentation/BaseStateViewModel.kt
@@ -0,0 +1,117 @@
+package kg.ruslan.testproject.core.presentation
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kg.ruslan.testproject.core.domain.Resource
+
+/**
+ * [BaseStateViewModel] is base class of your ViewModel which working with states
+ *
+ * @param UIState is generic which extend [BaseUIState]
+ * It's your UIState which you'll use in business logic
+ * @param UIIntents is generic of your UIIntents
+ * It's using to handle some events from View in ViewModel
+ * @param defaultState is default state of your [UIState] which will use to create it
+ * @constructor is creating instance of [BaseStateViewModel] and you can use some
+ * properties and member functions of it
+ * @author Ruslan
+ * @since version 0.0.1
+ */
+open class BaseStateViewModel<UIState : BaseUIState, UIIntents>(
+	private val defaultState: UIState,
+) : ViewModel() {
+	
+	/**
+	 * [_state] is [MutableStateFlow] with default value as [defaultState]
+	 * You able to use in to change state of your UI/View
+	 * This property created to manage [state]
+	 */
+	protected var _state: MutableStateFlow<UIState> =
+		MutableStateFlow(defaultState)
+	
+	/**
+	 * [state] is [StateFlow] which needs to be observed in a view
+	 * This state contains state of all your View
+	 */
+	var state = _state.asStateFlow()
+	
+	/**
+	 * [handleSideEffects] is a function to be called from View and handled in ViewModel
+	 * It contains some UI actions
+	 * You can handle these action in function [handleIntent]
+	 */
+	val handleSideEffects: (UIIntents) -> Unit
+	
+	/**
+	 * Progress. some loading
+	 */
+	private val _progressLV: MutableLiveData<Boolean> = MutableLiveData()
+	val progressLV: LiveData<Boolean> = _progressLV
+	
+	init {
+		handleSideEffects = { intent ->
+			viewModelScope.launch(Dispatchers.IO) {
+				handleIntent(intent = intent)
+				initialize()
+			}
+		}
+	}
+	
+	/**
+	 * Member function to initialize some property or do some logic in your viewModel
+	 */
+	protected open fun initialize() {}
+	
+	/**
+	 * [handleIntent] is member function to offer some [UIIntents] to the ViewModel
+	 * You need to override and implement this function
+	 *
+	 * @param intent is a [UIIntents] which is some actions from View
+	 */
+	protected open suspend fun handleIntent(intent: UIIntents) {}
+	
+	/**
+	 * [handleResourceInFlow] is a member function to observe [Flow] and process incoming data.
+	 *
+	 * @param resourceFlow is a [Flow] with some [Resource] type of [T]
+	 * This param will be observed to process incoming data: [Resource.Success], [Resource.Error], [Resource.Loading]
+	 * @param success is a higher-order function which will invoked if result got successfully
+	 * If something went wrong and result have not got successfully this function
+	 * will change [state] to report error by changing [BaseUIState.showError]
+	 * Also this function will manage loading state of [state]
+	 * @param emptySuccess is like [success] but without body
+	 */
+	protected suspend fun <T> handleResourceInFlow(
+		resourceFlow: Flow<Resource<T>>,
+		emptySuccess: ((Resource<Any>) -> Unit)? = null,
+		success: ((T) -> Unit)? = null,
+	) {
+		resourceFlow.collect { resource ->
+			when (resource) {
+				is Resource.Loading -> {
+					_progressLV.postValue(true)
+					_state.update { it.also { it.isLoading = true } }
+				}
+				
+				is Resource.Success -> {
+					emptySuccess?.invoke(Resource.Success(Any()))
+					success?.invoke(resource.data)
+					_progressLV.postValue(false)
+					_state.update { it.also { it.isLoading = false } }
+				}
+				
+				is Resource.Error -> {
+					_progressLV.postValue(false)
+					_state.update {
+						it.also { it.showError = resource.message; it.isLoading = false }
+					}
+				}
+			}
+		}
+	}
+}
