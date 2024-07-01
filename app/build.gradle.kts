97ad3a5aaf716c118c96467fb44e7592f9a13d69
Ruslan <ruslan@Ruslans-MacBook-Pro-2.local>
2024-07-01 18:49:51 +0600

    added util to read assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
 	implementation(libs.lifecycle.viewmodel.ktx)
 	implementation(libs.navigation.fragment.ktx)
 	implementation(libs.navigation.ui.ktx)
+	implementation(libs.timber)
 }
---
+++ app/src/main/java/kg/ruslan/testproject/utils/ReadJSONFromAssets.kt
@@ -0,0 +1,30 @@
+package kg.ruslan.testproject.utils
+
+import android.content.Context
+import timber.log.Timber
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun readJSONFromAssets(context: Context, path: String): String {
+    val identifier = "[ReadJSON]"
+	
+    try {
+        val file = context.assets.open(path)
+	    Timber.tag(identifier).i("Found File: $file.")
+        val bufferedReader = BufferedReader(InputStreamReader(file))
+        val stringBuilder = StringBuilder()
+        bufferedReader.useLines { lines ->
+            lines.forEach {
+                stringBuilder.append(it)
+            }
+        }
+	    Timber.tag(identifier).i("getJSON stringBuilder: $stringBuilder.")
+        val jsonString = stringBuilder.toString()
+	    Timber.tag(identifier).i(" JSON as String: $jsonString.")
+        return jsonString
+    } catch (e: Exception) {
+	    Timber.tag(identifier).e(" Error reading JSON: $e.")
+        e.printStackTrace()
+        return ""
+    }
+}
