ad86375e9f1c335ef29d4db414ef9e96e5e97872
Ruslan <ruslan@Ruslans-MacBook-Pro-2.local>
2024-07-01 17:48:15 +0600

    added data | added base repo | added resource model

---
+++ app/src/main/java/kg/ruslan/testproject/core/data/BaseRepo.kt
@@ -0,0 +1,21 @@
+package kg.ruslan.testproject.core.data
+
+import kg.ruslan.testproject.core.domain.Resource
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.flow
+
+open class BaseRepo {
+    protected fun <T> doRequest(block: suspend FlowCollector<Resource<T>>.() -> Unit) = flow {
+        emit(Resource.Loading())
+        try {
+            block()
+        } catch (e: Exception) {
+            emit(
+                Resource.Error(
+                    error = e,
+                    message = e.localizedMessage ?: "Details has not been provided"
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/kg/ruslan/testproject/core/domain/Resource.kt
@@ -0,0 +1,27 @@
+package kg.ruslan.testproject.core.domain
+
+/**
+ * [Resource] is using for transfer data between data and presentation layer
+ *
+ * @param data is some result from local or remote data source.
+ * @param message is message from some error by trying get data from local/remote data source
+ */
+sealed class Resource<out T>(
+    open val data: T? = null,
+    open val message: String? = null,
+) {
+    /**
+     * [Loading] intermediate value before get data or error
+     */
+    class Loading<T>(data: T? = null) : Resource<T>(data = data)
+
+    /**
+     * [Success] success response from data source
+     */
+    class Success<T>(override val data: T) : Resource<T>(data = data)
+
+    /**
+     * [Error] result got wrong. Error is state with message te notify receiver that something went wrong
+     */
+    class Error<T>(override val message: String, data: T? = null, val error: Throwable? = null) : Resource<T>(data = data, message = message)
+}
